720e64f1e525cc560813e5bbeec4601b8be2f529
Mahmudul Hasan <mahmudnotes@outlook.com>
2023-04-18 04:06:38 +0600

    removed all

--- Domain/Entities/ExpenseType.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Primitive;
-
-namespace Domain.Entities;
-
-public class ExpenseType : Entity
-{
-    public string Name { get; set; }
-    public List<FinancialRecord> FinancialRecords { get; set; }
-}
--- Domain/Entities/IncomeType.cs
+++
@@ -1,9 +0,0 @@
-﻿using Domain.Primitive;
-
-namespace Domain.Entities;
-
-public class IncomeType : Entity
-{
-    public string Name { get; set; }
-    public List<FinancialRecord> FinancialRecords { get; set; }
-}
